af03dd8679239b0a9405e2476e9d684d2e763f37
Banapel <93161288+Banapel@users.noreply.github.com>
2022-03-14 10:12:27 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,16 @@
 ﻿//////////////////////////////////////////////////////////////////
 // Jasper Jung
 // TINFO-200C
-// L5life - Game of Life Assignment
-// This program shows a simulation of populations using a top 
-// down approach. This will be done using 2 dimensional arrays 
-// and nested for loops. The requirements include that a cell is born 
-// if it has exactly three  neighbors and survives if there are two or 
-// three living neighbors or else it dies. 
+// L1mpg - First Lab Assignment
+// This program takes in user input on their weight in pounds, heigh in inches, 
+// and then calculate their BMI. Information from the Department of Health and Human 
+// services are also shown to help the user determine what their bmi means
+// body mass index. 
 /////////////////////////////////////////////////////////////////
 // Change History
 // Date     Developer       Description of Change
-// 1/30/2022  Jasper Jung   Added description of the program and started creating each file 
-// 2/2/2022  Jasper Jung    filled in each method and started testing
+// 1/20/2022  Jasper Jung     File and Project created - implementation and testing
+// 1/20/2022  Jasper Jung     Added brief description
 
 using System;
 using System.Collections.Generic;
@@ -19,15 +18,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace L5Life
+// BodyMass namespace holds the solutions project for the BMI calculations
+namespace BodyMass
 {
+    // main class that holds the BodyMass program
     internal class Program
     {
+        
+        // main driver of the program that asks the user what to input
         static void Main(string[] args)
         {
-            // game object is created to start simulation
-            Game game = new Game();
-            game.StartGame();
+            Introduction();
+            Console.WriteLine("Enter your weight in pounds (eg. 130, 140): ");
+            int weightPounds = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your height in inches (eg. 50, 65): ");
+            int heightInches = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your current BMI is: " + BMICalculator(weightPounds, heightInches));
+            BMIStats();
         }
+
+        // tells the user what the program does
+        protected static void Introduction()
+        {
+            Console.WriteLine(@"
+This program takes in your weight in pounds and height in inches. This information is then used 
+to calucluate your bmi. Using your BMI number, you can then compare it to information from the 
+Department of Health and Human Services/National Institutes of Health to see where your health 
+is at. ");
+        }
+       
+        // this method calculates the user's BMI from their inputted weight and height
+        protected static int BMICalculator(int weightPounds, int heightInches)
+        {
+            // BMI variable
+            int userBMI;
+            // equation to calculate the bmi of the user
+            userBMI = (weightPounds * 703) / (heightInches * heightInches);
+            // return the BMI value
+            return userBMI;
+        }
+        // this method resents information on BMI from the Department of health https://www.nhlbi.nih.gov/health/educational/lose_wt/BMI/bmicalc.htm
+        protected static void BMIStats()
+        {
+            Console.WriteLine(@"
+-------------------------------------------
+*               BMI Values                *
+*   Underweight:  less than 18.5          *
+*   Normal:       between 18.5 and 24.9   *
+*   Overweight:   between 25 and 29.9     *
+*   Obese:        30 or greater           *
+-------------------------------------------");
+        }
+  
     }
-}
+}   
